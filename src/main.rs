e1b4fd5c97d35988c2a0b358cef6db75943df35f
pedrojjsantos <pedrojjsantos.cc@gmail.com>
2020-03-14 16:23:23 -0300

    editing the main's error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 mod zip;
+//mod unzip;
+
 use std::fs;
 
 fn main() -> std::io::Result<()> { 
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
-        panic!("no arguments used");
-    }
-    let mut file = fs::File::open(&args[1])?;
-    let option = &args[2];
-    
-    match &option[..] {
-        "-c" => zip::compress(&mut file),
-        //"-d" => {}
-        _ => panic!( "not a valid option:\n
-                     '-c' to compress\n
-                     '-d' to decompress"),
+    match args.len() {
+        1 => print!("no arguments used, the arguments must be:\n<filename or path> <-c or -d>\n"),
+        2 => print!("please especify an option:\n  -c to compress\n  -d to decompress\n"),
+        _ => {
+            let file = fs::File::open(&args[1])?;
+            let option = &args[2];
+            
+            match &option[..] {
+                "-c" => zip::compress(file)?,
+                "-d" => println!("decompressing"),// unzip::decompress(file),
+                e => print!("'{}' is not a valid option:\n  '-c' to compress\n  '-d' to decompress\n", e),
+            }
+        }
     }
 
+    Ok(())
 }
