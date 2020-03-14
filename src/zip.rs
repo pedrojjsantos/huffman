a7dc466b95a493a66c35a57d35c76f9a2be7b73c
pedrojjsantos <pedrojjsantos.cc@gmail.com>
2020-03-14 16:25:28 -0300

    rewriting the zip functions from zero

--- src/zip.rs
+++ src/zip.rs
@@ -1,178 +1,159 @@
 #[path = "adt_huffman.rs"]
 mod adt_huffman;
 
-use adt_huffman::{Heap, Tree};
+use adt_huffman::{Tree, Heap};
 use std::fs;
 use std::io::{Read, Write, Seek, SeekFrom};
 
-pub fn compress(file: &mut std::fs::File) -> std::io::Result<()> {
-    let mut frequency: Vec<u8> = vec![0;256];
+pub fn compress(mut file: fs::File) -> std::io::Result<()> {
+    let mut frequency: Vec<u64> = vec![0;256];
+    let f = Read::by_ref(&mut file);
+
+    for i in f.bytes() {
+        let b = i.unwrap() as usize;
+
+        frequency[b] += 1;
+    }
+    file.seek(SeekFrom::Start(0))?;
+
+    let huff_tree = build_huffman_tree(&frequency);
+    let mut preorder: Vec<u8> = Vec::new();
+
+    get_preorder(&huff_tree, &mut preorder);
+
+    let mut binary: Vec<String> = vec![String::new();256];
 
     {
-        let mut buffer = String::new();
-        file.read_to_string(&mut buffer)?;
-
-        for byte in buffer.bytes() {
-            let i = byte as usize;
-            frequency[i] += 1;
-        }
-    }
-    
-    let huffman_tree = build_huffman_tree(&frequency);
-    
-    let mut binary = vec![String::new();256];
-    let mut preorder_tree = String::new();
-    
-    create_preorder(&huffman_tree, &mut preorder_tree);
-
-    {
-        let mut string = String::new();
-        create_new_binary(huffman_tree, &mut string, &mut binary);
+        let mut buf = String::new();
+        new_binary(&huff_tree, &mut binary, &mut buf);
     }
 
-    let mut new_size: usize = 0;
+    let mut new = fs::File::create("tests/compressed.huff")?;
 
-    for (byte, f) in frequency.iter().enumerate() {
-        let size = *f as usize;
-        new_size += size * binary[byte].len();
-    }
+    let trash = write_file(&file, &mut new, &binary, &preorder).unwrap();
 
-    let mut trash_size = 8 - (new_size % 8) as u8;
-    if trash_size == 8 {
-        trash_size = 0 as u8;
-    }
-
-    let mut new_file = fs::File::create("tests/compressed.huff")?;
-
-    write_header(&mut new_file, preorder_tree, trash_size)?;
-    write_new_file(&mut new_file, file, &binary)?;
+    write_header(&mut new, trash, preorder.len() as u16)?;
 
     Ok(())
 }
 
-fn build_huffman_tree(frequency: &Vec<u8>) -> Tree<u8> {
+fn build_huffman_tree(frequency: &Vec<u64>) -> Tree {
     let mut heap = Heap::new();
 
-    for (i, f) in frequency.iter().enumerate() {
+    for (b, f) in frequency.iter().enumerate() {
         if *f > 0 {
-            let tree = Tree::new(i as u8, *f as i64);
-            heap.enqueue(tree);
+            heap.enqueue(Tree::new(b as u8, *f));
         }
     }
 
-    if heap.data.len() == 1 {
-        let left = heap.dequeue().unwrap();
-        let mut new = Tree::new(b'*', left.freq);
+    while heap.len() > 1 {
+        let l = heap.dequeue().unwrap();
+        let r = heap.dequeue().unwrap();
+        let mut new = Tree::new(b'*', l.freq + r.freq);
 
-        new.add_left(left);
-
-        return new;
-    }
-
-    while heap.data.len() > 1 {
-        let left = heap.dequeue().unwrap();
-        let right = heap.dequeue().unwrap();
-
-        let mut new: Tree<u8> = Tree::new(b'*', left.freq + right.freq);
-
-        new.add_left(left);
-        new.add_right(right);
-
+        new.add_left(l);
+        new.add_right(r);
         heap.enqueue(new);
     }
 
     heap.dequeue().unwrap()
 }
 
-fn create_preorder(tree: &Tree<u8>, preorder: &mut String) {
-    if tree.is_leaf() {
-        match tree.item {
-            b'\\' => preorder.push_str("\\\\"),
-            b'*'  => preorder.push_str("\\*"),
-            x     => preorder.push(x as char),
-        }
-        return;
-    }
-
-    preorder.push('*');
-
-    if tree.left.is_some() {
-        let left = tree.left.as_ref();
-        create_preorder(left.unwrap(), preorder);
-    }
-    if tree.right.is_some() {
-        let right = tree.right.as_ref();
-        create_preorder(right.unwrap(), preorder);
-    }
-}
-
-fn create_new_binary(tree: Tree<u8>, string: &mut String, binary: &mut Vec<String>) {
+fn new_binary(tree: &Tree, binary: &mut Vec<String>, buf: &mut String) {
     if tree.is_leaf() {
         let i = tree.item as usize;
-        binary[i] = string.clone();
-
+        binary[i].clone_from(&buf);
         return;
     }
-    let len = string.len();
 
-    if tree.left.is_some() {
-        string.push('0');
-        create_new_binary(*tree.left.unwrap(), string, binary);
-    }
-    string.truncate(len);
-    
-    if tree.right.is_some() {
-        string.push('1');
-        create_new_binary(*tree.right.unwrap(), string, binary);
-    }
-    string.truncate(len);
+    let left = tree.left.as_ref().unwrap();
+    buf.push('0');
+    new_binary(left, binary, buf);
+    buf.pop();
+
+    let right = tree.right.as_ref().unwrap();
+    buf.push('1');
+    new_binary(right, binary, buf);
+    buf.pop();
 }
 
-fn write_header(new_file: &mut fs::File, preorder: String, trash_size: u8) -> std::io::Result<()> {
-    let mut header: [u8;2] = [0, 0];
+fn get_preorder(tree: &Tree, preorder: &mut Vec<u8>) {
+    if tree.is_leaf() {
+        match tree.item {
+            b'*'  => {
+                preorder.push(b'\\');
+                preorder.push(b'*');
+            }
+            b'\\' => {
+                preorder.push(b'\\');
+                preorder.push(b'\\');
+            }
+            item  => preorder.push(item),
+        }
+        return;
+    }
 
-    header[0] = trash_size << 5;
-    header[0] |= (preorder.len() >> 8) as u8;
-    header[1] = preorder.len() as u8;
+    preorder.push(tree.item);
 
-    new_file.write(&header[..])?;
-    new_file.write(preorder.as_bytes())?;
+    let left  = tree.left.as_ref().unwrap();
+    let right = tree.right.as_ref().unwrap();
 
-    Ok(())
+    get_preorder(left, preorder);
+    get_preorder(right, preorder);
 }
 
-fn write_new_file(new_file: &mut fs::File, old_file: &mut fs::File, binary: &Vec<String>) -> std::io::Result<()> {
+fn write_file(old: &fs::File, new: &mut fs::File, binary: &Vec<String>, preorder: &Vec<u8>) -> std::io::Result<u8> {
     let mut byte: [u8;1] = [0];
     let mut index: i8 = 7;
+    let mut flag: bool = false;
 
-    old_file.seek(SeekFrom::Start(0))?;
+    new.write(&byte)?;
+    new.write(&byte)?;
+    new.write(preorder)?;
 
-    for i in old_file.bytes() {
-        let b = i.unwrap();
-        let b = b as usize;
-        
-        for j in binary[b].bytes() {
+    for i in old.bytes() {
+        let i = i.unwrap() as usize;
+
+        for j in binary[i].bytes() {
+            if j == b'1' {
+                byte[0] = set_bit(index, byte[0]);
+            }
             
-            if index == -1 {
-                new_file.write(&byte[..])?;
+            flag = true;
+            index -= 1;
+            
+            if index < 0 {
+                new.write(&byte)?;
                 byte[0] = 0;
                 index = 7;
+                flag = false;
             }
-            if j == b'1' {
-                byte[0] = set_bit(&byte[0], &index);
-            }
-            index -= 1;
         }
     }
 
-    if byte[0] != 0 {
-        new_file.write(&byte[..])?;
+    if flag {
+        new.write(&byte)?;
     }
+
+    let trash = if index == 7 {0} else {index + 1};
+
+    Ok(trash as u8)
+}
+
+fn write_header(file: &mut fs::File, trash: u8, tree_size: u16) -> std::io::Result<()> {
+    let mut header: [u8;2] = [0,0];
     
+    header[0] = trash << 5;
+    header[0] |= (tree_size >> 8) as u8;
+    header[1] = tree_size as u8;
+
+    file.seek(SeekFrom::Start(0))?;
+    file.write(&header)?;
+
     Ok(())
 }
 
-fn set_bit(item: &u8, i: &i8) -> u8 {
-    let mask: u8 = 1 << *i;
-    item | mask
+fn set_bit(i: i8, x: u8) -> u8 {
+    let mask: u8 = 1 << i;
+    x | mask
 }
